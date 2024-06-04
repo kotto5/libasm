5c5900e1a3225d5f2765c23e5c985ced1e84fd7d
kotto5 <kotto555555@gmail.com>
2024-06-04 11:39:54 +0900

    ft_read.asm

---
+++ ft_read.asm
@@ -0,0 +1,30 @@
+extern __errno_location
+
+section .text
+global ft_read
+
+; ssize_t ssize_t read(int fildes, void *buf, size_t nbyte);
+;                     (rdi       , rsi      , rdx         )
+
+; sycall (syscall_id, fd, buf, nbyte)
+;        (rax       ,rdi, rsi, rdx  )
+
+; rdi -> rdi
+; rsi -> rsi
+; rdx -> rdx
+; rax -> 1
+
+ft_read:
+    mov     rax, 0
+    syscall
+    cmp rax, 0
+    jl set_errno
+    ret
+
+set_errno:
+    mov rsi, rax
+    call __errno_location
+    mov [rax], esi
+    ; TODO rax に -1 を入れる
+    mov rax, -1
+    ret
