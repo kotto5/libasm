841f08e4c0dd75e87ae4be118215157f24ed24ba
kakiba <97882386+kotto5@users.noreply.github.com>
2024-06-12 07:56:36 +0000

    部分に分けて cの関数使いながら挙動テスト中

--- ft_list_sort.s
+++ ft_list_sort.s
@@ -1,10 +1,11 @@
-section .data
-    ; データセグメントはここでは使いません
-
 extern ft_list_size
+extern ft_list_sort_one_liner
 
 section .text
     global ft_list_sort
+    global swap
+    global rotate
+    global proceed_next
 
 swap:
     ; 引数:
@@ -57,8 +58,6 @@ rotate:
     pop rbp             ; ベースポインタを復元
     ret                 ; 関数から戻る
 
-section .text
-    global proceed_next
 
 proceed_next:
     ; 引数:
@@ -67,108 +66,120 @@ proceed_next:
     ; rdx: _3rd
 
     ; プロローグ
-    push rbp            ; ベースポインタを保存
-    mov rbp, rsp        ; ベースポインタを設定
+    push rbp
+    mov rbp, rsp
+    push rbx
 
-    ; *_1st = &(**_1st)->next;
+    ; *_1st = &(**_1st)->next
     mov rax, [rdi]      ; rax = *_1st
     mov rax, [rax]      ; rax = **_1st
-    mov rax, [rax]      ; rax = (**_1st)->next
+    lea rax, [rax+8]    ; rax = &(**_1st)->next
     mov [rdi], rax      ; *_1st = rax
 
-    ; *_2nd = &(**_1st)->next;
+    ; *_2nd = &(**_1st)->next
     mov rax, [rdi]      ; rax = *_1st
     mov rax, [rax]      ; rax = **_1st
-    mov rax, [rax]      ; rax = (**_1st)->next
+    lea rax, [rax+8]    ; rax = &(**_1st)->next
     mov [rsi], rax      ; *_2nd = rax
 
-    ; *_3rd = &(**_2nd)->next;
+    ; *_3rd = &(**_2nd)->next
     mov rax, [rsi]      ; rax = *_2nd
     mov rax, [rax]      ; rax = **_2nd
-    mov rax, [rax]      ; rax = (**_2nd)->next
+    lea rax, [rax+8]    ; rax = &(**_2nd)->next
     mov [rdx], rax      ; *_3rd = rax
 
     ; エピローグ
-    pop rbp             ; ベースポインタを復元
-    ret                 ; 関数から戻る
-
-ft_list_sort_one_liner:
-    ; 引数:
-    ; rdi: begin_list
-    ; rsi: cmp
-
-    ; プロローグ
-    push rbp
-    push rbx
-    push r12
-    push r13
-    push r14
-    push r15
-
-    ; 引数をレジスタから保存
-    mov r12, rdi  ; r12 = begin_list
-    mov r13, rsi  ; r13 = cmp
-
-    ; if (!begin_list || !*begin_list)
-    mov rax, [r12]
-    test rax, rax
-    jz .done
-
-    ; t_list **_1st = begin_list;
-    mov r14, r12  ; r14 = _1st
-
-.loop_start:
-    ; proceed_next(&_1st, &_2nd, &_3rd);
-    lea rdi, [r14]    ; 引数1: &_1st
-    lea rsi, [rsp-8]  ; 引数2: &_2nd（スタックに一時的に保存）
-    lea rdx, [rsp-16] ; 引数3: &_3rd（スタックに一時的に保存）
-    call proceed_next
-
-    ; _2ndと_3rdをスタックから取得
-    mov r15, [rsp-8]  ; r15 = _2nd
-    mov rbx, [rsp-16] ; rbx = _3rd
-
-    ; if (cmp((*_1st)->data, (*_2nd)->data) > 0)
-    mov rdi, [r14]    ; rdi = *_1st
-    mov rdi, [rdi+8]  ; rdi = (*_1st)->data
-    mov rsi, [r15]    ; rsi = *_2nd
-    mov rsi, [rsi+8]  ; rsi = (*_2nd)->data
-    mov rax, r13      ; 関数ポインタcmpをraxに移動
-    call rax          ; cmp((*_1st)->data, (*_2nd)->data)
-
-    ; 結果をチェック
-    test eax, eax
-    jle .no_swap
-
-    ; rotate(_1st, _2nd, _3rd);
-    mov rdi, r14      ; 引数1: _1st
-    mov rsi, r15      ; 引数2: _2nd
-    mov rdx, rbx      ; 引数3: _3rd
-    call rotate
-
-    ; swap(&_2nd, &_3rd);
-    lea rdi, [rsp-8]  ; 引数1: &_2nd
-    lea rsi, [rsp-16] ; 引数2: &_3rd
-    call swap
-
-.no_swap:
-    ; if (*_3rd == NULL) break;
-    mov rax, [rbx]
-    test rax, rax
-    jz .done
-
-    jmp .loop_start
-
-.done:
-    ; レジスタをスタックから復元
-    pop r15
-    pop r14
-    pop r13
-    pop r12
     pop rbx
     pop rbp
-
     ret
+
+; ft_list_sort_one_liner:
+;     ; 引数:
+;     ; rdi: begin_list
+;     ; rsi: cmp
+
+;     ; プロローグ
+;     push rbp
+;     mov rbp, rsp
+;     push rbx
+;     push r12
+;     push r13
+;     push r14
+;     push r15
+
+;     ; 引数をレジスタから保存
+;     mov r12, rdi  ; r12 = begin_list
+;     mov r13, rsi  ; r13 = cmp
+
+;     ; if (!begin_list || !*begin_list)
+;     mov rax, [r12]
+;     test rax, rax
+;     jz .done
+
+;     ; t_list **_1st = begin_list;
+;     mov r14, r12  ; r14 = _1st
+;     ; t_list **_2nd = &(*_1st)->next;
+;     mov rbx, [r14]
+;     mov rbx, [rbx]
+;     lea r15, [rbx+8] ; r15 = &(*_1st)->next (rbx + 8 offset for 'next')
+
+;     ; t_list **_3rd = &(*_2nd)->next;
+;     mov rbx, [r15]
+;     mov rbx, [rbx]
+;     lea rdx, [rbx+8] ; rdx = &(*_2nd)->next
+
+; ._liner_loop_start:
+;     jmp .check_cmp
+
+; .check_cmp:
+;     ; if (cmp((*_1st)->data, (*_2nd)->data) > 0)
+;     mov rdi, [r14]
+;     mov rdi, [rdi+8]
+;     mov rsi, [r15]
+;     mov rsi, [rsi+8]
+;     mov rax, r13
+;     call rax
+;     test eax, eax
+;     jle .no_swap
+
+;     ; rotate(_1st, _2nd, _3rd);
+;     mov rdi, r14
+;     mov rsi, r15
+;     mov rdx, rdx
+;     call rotate
+
+;     ; swap(&_2nd, &_3rd);
+;     lea rdi, [rsp-8]
+;     lea rsi, [rsp-16]
+;     call swap
+
+; .no_swap:
+;     ; if (*_3rd == NULL) break;
+;     mov rax, [rdx]
+;     test rax, rax
+;     jz .done
+
+;     ; proceed_next(&_1st, &_2nd, &_3rd);
+;     lea rdi, [r14]
+;     lea rsi, [rsp-8]
+;     lea rdx, [rsp-16]
+;     call proceed_next
+
+;     ; _2ndと_3rdをスタックから取得
+;     mov r15, [rsp-8]
+;     mov rdx, [rsp-16]
+
+;     jmp ._liner_loop_start
+
+; .done:
+;     ; レジスタをスタックから復元
+;     pop r15
+;     pop r14
+;     pop r13
+;     pop r12
+;     pop rbx
+;     pop rbp
+;     ret
 
 ; ft_list_sort関数の定義
 ft_list_sort:
